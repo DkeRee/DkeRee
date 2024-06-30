d14052d2819519fc00993355d280fe573e800a77
Daniel Ke <dkeree1@gmail.com>
2024-06-30 16:22:15 -0700

    added vongle

--- pages/trinket/widgets.js
+++
@@ -1,5 +0,0 @@
-var playButton = document.getElementsByClassName("play-button")[0];
-
-playButton.addEventListener("mousedown", () => {
-    window.open("https://github.com/DkeRee/Trinket", "_blank");
-});
--- pages/wanshot/widgets.js
+++
@@ -1,5 +0,0 @@
-var playButton = document.getElementsByClassName("play-button")[0];
-
-playButton.addEventListener("mousedown", () => {
-    window.open("https://dkeree.github.io/Wanshot", "_blank");
-});
