afcd20756a14de8ecbedea0b82278af18eaeeb94
Daniel Ke <dkeree1@gmail.com>
2024-06-30 16:42:21 -0700

    add snerkle

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,12 @@
 const content = document.getElementById("content");
 const buttons = document.getElementsByClassName("selector-button");
-const sublinks = ["aboutme/aboutme", "wanshot/wanshot", "trinket/trinket", "vongle/vongle"];
+const sublinks = ["aboutme/aboutme", "wanshot/wanshot", "trinket/trinket", "vongle/vongle", "snerkle/snerkle"];
 
 //initialize page content to default to About Me
 $(content).load("pages/aboutme/aboutme.html");
 
 
-for (var i = 0; i < 4; i++) {
+for (var i = 0; i < 5; i++) {
 	const link = sublinks[i];
 
 	buttons[i].addEventListener("mousedown", () => {
