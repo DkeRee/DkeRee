ee9dad9238ad2c2124f470671446e6264e042235
Daniel Ke <dkeree1@gmail.com>
2024-06-30 17:33:47 -0700

    fix boxgle add corcle and shoutout template

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,12 @@
 const content = document.getElementById("content");
 const buttons = document.getElementsByClassName("selector-button");
-const sublinks = ["aboutme/aboutme", "wanshot/wanshot", "trinket/trinket", "vongle/vongle", "snerkle/snerkle", "arcadia/arcadia", "boxgle/boxgle"];
+const sublinks = ["aboutme/aboutme", "wanshot/wanshot", "trinket/trinket", "vongle/vongle", "snerkle/snerkle", "arcadia/arcadia", "boxgle/boxgle", "corcle/corcle", "shoutouts/shoutouts"];
 
 //initialize page content to default to About Me
 $(content).load("pages/aboutme/aboutme.html");
 
 
-for (var i = 0; i < 7; i++) {
+for (var i = 0; i < buttons.length; i++) {
 	const link = sublinks[i];
 
 	buttons[i].addEventListener("mousedown", () => {
