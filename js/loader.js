6f0648fab351de86e20e4322a3ae7ba77ec48836
Daniel Ke <dkeree1@gmail.com>
2024-06-30 17:29:48 -0700

    added boxgle

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,12 @@
 const content = document.getElementById("content");
 const buttons = document.getElementsByClassName("selector-button");
-const sublinks = ["aboutme/aboutme", "wanshot/wanshot", "trinket/trinket", "vongle/vongle", "snerkle/snerkle", "arcadia/arcadia"];
+const sublinks = ["aboutme/aboutme", "wanshot/wanshot", "trinket/trinket", "vongle/vongle", "snerkle/snerkle", "arcadia/arcadia", "boxgle/boxgle"];
 
 //initialize page content to default to About Me
 $(content).load("pages/aboutme/aboutme.html");
 
 
-for (var i = 0; i < 6; i++) {
+for (var i = 0; i < 7; i++) {
 	const link = sublinks[i];
 
 	buttons[i].addEventListener("mousedown", () => {
