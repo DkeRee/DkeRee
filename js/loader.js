b4505fd3e5408540964c4d2faf678d711578a424
Daniel Ke <dkeree1@gmail.com>
2024-06-30 17:08:13 -0700

    added arcadia

--- js/loader.js
+++ js/loader.js
@@ -1,12 +1,12 @@
 const content = document.getElementById("content");
 const buttons = document.getElementsByClassName("selector-button");
-const sublinks = ["aboutme/aboutme", "wanshot/wanshot", "trinket/trinket", "vongle/vongle", "snerkle/snerkle"];
+const sublinks = ["aboutme/aboutme", "wanshot/wanshot", "trinket/trinket", "vongle/vongle", "snerkle/snerkle", "arcadia/arcadia"];
 
 //initialize page content to default to About Me
 $(content).load("pages/aboutme/aboutme.html");
 
 
-for (var i = 0; i < 5; i++) {
+for (var i = 0; i < 6; i++) {
 	const link = sublinks[i];
 
 	buttons[i].addEventListener("mousedown", () => {
